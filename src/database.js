bd00e40f4760b3d9a7b893f16309309b3e357350
Eythan <eythan.cvt@gmail.com>
2021-05-27 03:55:29 -0500

    ya nose x2

--- src/database.js
+++ src/database.js
@@ -2,16 +2,16 @@ const mysql = require('mysql');
 const { promisify } = require('util');
 const { database } = require('./keys');
 
-const pool = mysql.createConnection(database);
+const dbSocketAddr = process.env.DB_HOST.split(':');
 
-pool.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
- 
-  console.log('connected as id ' + pool.threadId);
-});
+const pool = mysql.createPool({
+    user: process.env.DB_USER, // e.g. 'my-db-user'
+    password: process.env.DB_PASS, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    host: dbSocketAddr[0], // e.g. '127.0.0.1'
+    port: dbSocketAddr[1], // e.g. '3306'
+    // ... Specify additional properties here.
+  });
 
 pool.query = promisify(pool.query);
 module.exports = pool;
