39513c09b4e26a9d226c2b1c93cde547a17417d6
Eythan <eythan.cvt@gmail.com>
2021-05-29 20:26:35 -0500

    v8.0.9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan');
+//const morgan = require('morgan'); // desactivar en prod
 const cors = require('cors');
 const path = require('path');
 const passport = require('passport');
@@ -16,24 +16,18 @@ require('./lib/passport');
 app.set('port', process.env.PORT || 3000);
 
 // MiddLewares
-app.use(morgan('common'));
+//*app.use(morgan('common'));    // desactivar en prod
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
 
 // Global Variables
-app.use((req, res, next) => {
-  next();
-});
-
-// Publics
 
 
 // Routes
 
 app.use('/api/v1', require('./routes/api.js'), require('./routes/api-pay.js'));
-
 
 
 app.use(customMdw.errorHandler);
